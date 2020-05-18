bba90210e63493061a3b4bf02e9229d4a6d9e832
gux <gux.vega@gmail.com>
2020-05-18 12:31:31 -0600

    adding sign in form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import HomePage from './pages/homepages/homepage.component';
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
 function App() {
 	return (
@@ -13,6 +14,8 @@ function App() {
 			<Switch>
 				<Route exact path="/" component={HomePage} />
 				<Route path="/shop" component={ShopPage} />
+				<Route path="/signin" component={SignInAndSignUp} />
+
 			</Switch>
 		</div>
 	);
